7365ea16cf86b4a13e86253efab5dd9ff927aca7
liyp <365859350@qq.com>
2017-07-27 21:31:44 +0800

    1. 修改buffer 读写，buffer 大小反馈读取 2. 添加block 解码 3. 添加pointcloud2 格式转换

--- rfans_driver/src/bufferDecode.h
+++ rfans_driver/src/bufferDecode.h
@@ -1,9 +1,12 @@
 #ifndef _BUFFER_DECODER_H_
 #define _BUFFER_DECODER_H_
+//#include <pcl_ros/point_cloud.h>
+#include <sensor_msgs/PointCloud2.h>
 #include "rfans_driver/RfansPacket.h"
 #include "ssFrameLib.h"
 
-static const int DECODE_BUFFER_SIZE = 0x20000; // 128KB
+
+static const int DECODE_BUFFER_SIZE = 0x40000; // 256KB
 typedef struct {
   int wrHead, rdTail, bufSize;
   unsigned char buffer[DECODE_BUFFER_SIZE];
@@ -20,16 +23,34 @@ class SSBufferDec {
 public:
   SSBufferDec();
   ~SSBufferDec();
+  int moveWriteIndex(int setpIndex);
+  int moveReadIndex(int setpIndex);
+  unsigned char * getWriteIndex();
+  unsigned char * getReadIndex();
+
   int writeBuffer(unsigned char *data, int size);
+
   int readPacket(rfans_driver::RfansPacket &pkt);
+  int readPacket(std::vector<SCDRFANS_BLOCK_S> &outBlocks);
+
+  int readPacketStream(rfans_driver::RfansPacket &pkt);
+  int size();
+  int freeSize();
   void reset();
 
   static int Depacket(rfans_driver::RfansPacket &inPack, std::vector<SCDRFANS_BLOCK_S> &outBlocks);
   static int ConvertXyz(std::vector<SCDRFANS_BLOCK_S> &inBlocks, std::vector<RFANS_XYZ_S> &outXyzBlocks);
+
+  static int ConvertPointCloud(std::vector<SCDRFANS_BLOCK_S> &inBlocks, std::vector<RFANS_XYZ_S> &inXyzBlock,
+                               sensor_msgs::PointCloud2 &outCloud) ;
+
+  static void InitPointcloud2(sensor_msgs::PointCloud2 &initCloud) ;
+  static void ResetPointCloud2(sensor_msgs::PointCloud2 &initCloud) ;
 private:
   int findPacketPro();
   int findZeroPro();
   int readBuffer() ;
+  int readBufferSteam() ;
   int ouputPacket(rfans_driver::RfansPacket &pkt);
   void bufferReverse();
 private:
@@ -37,6 +58,7 @@ private:
   DEC_STSTUS_E m_status ;
   rfans_driver::RfansPacket m_packet ;
   int m_packetSize ;
+  int m_blockCout ;
   float s_preAngle ;
 };
 
